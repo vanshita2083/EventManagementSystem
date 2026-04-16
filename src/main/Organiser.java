24ea521c37259765abb1299784dd0f27e349170b
vanshita2083 <shahvanshita2083@gmail.com>
2026-04-16 01:05:38 -0400

    Organiser Final update

--- src/main/Organiser.java
+++ src/main/Organiser.java
@@ -2,4 +2,46 @@ package main;
 
 public class Organiser {
 
+    private int organiserId;
+    private String name;
+    private String contactInfo;
+
+    public Organiser() {
+    }
+
+    public Organiser(int organiserId, String name, String contactInfo) {
+        this.organiserId = organiserId;
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+
+    public int getOrganiserId() {
+        return organiserId;
+    }
+
+    public void setOrganizerId(int organiserId) {
+        this.organiserId = organiserId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String toString() {
+        return "Organizer ID: " + organiserId +
+                ", Name: " + name +
+                ", Contact: " + contactInfo;
+    }
 }
